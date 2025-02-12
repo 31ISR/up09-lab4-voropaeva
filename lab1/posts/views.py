55188f1b1794c31942c814662dae73ca163304f6
zhenyal123 <zhmono77@gmail.com>
2025-02-12 04:16:52 +0000

    3 пункт 4

--- lab1/posts/views.py
+++ lab1/posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    return render(request, 'posts/post_new.html')
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
